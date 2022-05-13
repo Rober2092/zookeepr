da0e67a24bbba710a2ed7c2aa1b6a14e2ca934b1
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2022-05-12 22:52:28 -0700

    Add files and code

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,15 +3,15 @@ const router = require('express').Router();
 
 // look for and host index.html
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../lib/public/index.html'));
 });
 
 router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+    res.sendFile(path.join(__dirname, '../../lib/public/animals.html'));
 });
 
 router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+    res.sendFile(path.join(__dirname, '../../lib/public/zookeepers.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const app = express();
 const { animals } = require('./data/animals');
-app.listen(3001, () => {
-    app.get('/api/animals', (req, res) => {
-        res.json(animals);
-      });
-       
-    
-    console.log(`API server now on port 3001!`);
-  });
-  
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// make all static files accessible from server
+app.use(express.static('public'));
+
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// Parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
